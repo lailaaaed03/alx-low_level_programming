5983bccb089a1965975a0229c6d52e59e3558e4c
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-08 16:45:31 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * a c program that prints exactly a sentence
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
