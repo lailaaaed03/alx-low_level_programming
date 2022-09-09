d0d5f4541983847bc0eb3e9bc10284fc407d7184
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-09 16:33:49 -0700

    lastt taskss

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n status; zero,p,n
+ *
+ * Return: Always 0(Success)
+ */
+
+
+int main(void)
+{
+
+int i;
+	for (i = 48; i <= 102; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			i += 39;
+	}
+
+	printf("\n");
+	return (0);
+}
