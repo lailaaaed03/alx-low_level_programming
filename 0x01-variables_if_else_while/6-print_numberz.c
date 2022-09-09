27be1b572666b448ab90bdb0e8ea2e5f0aa86142
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-09 16:03:47 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the digit number of base 10 ; 
+ *
+ * Return: Always 0(Success)
+ */
+
+
+int main(void)
+{
+
+int i;
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	putchar('\n');
+	return (0);
+}
