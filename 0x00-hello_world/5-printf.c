9edaaf6a5a0c0d8088e8b4c32443268596ebcbe0
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-08 17:00:54 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * a c program that prints the exact sentence 
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
