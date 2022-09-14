3f44ad02f3ba0956b5a66de6881b3732c6969a26
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-14 15:39:14 -0700

    taskkk

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that check if  the alphabet is lowecase or upper
+ *
+ *@c: checks input
+ *
+ *Return: return 1 if true, else 0
+ *
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) ||
+	    (c >= 65 && c <= 90))
+		return (1);
+	return (0);
+}
