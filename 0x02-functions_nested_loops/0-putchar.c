f5ab730346adefd404ab2cba96720623800ee427
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-14 09:58:35 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print value of n status; zero,p,n
+ *
+ * Return: Always 0(Success)
+ */
+
+
+int main(void)
+{
+char ch[] = "_putchar";
+int i;
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+return (0);
+}
