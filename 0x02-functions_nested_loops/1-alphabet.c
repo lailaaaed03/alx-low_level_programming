52e81958d321465bd2f64ef5a98dd6bd9aa75528
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-14 10:10:19 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - use printchar to print lowercase number
+ */
+
+void print_alphabet(void)
+{
+int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
