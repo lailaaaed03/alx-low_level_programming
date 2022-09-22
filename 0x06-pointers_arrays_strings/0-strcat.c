d19bc3a7f828f98b568b3f13f98c086f4b33969f
lailaaaed03 <lailaeddaris03@gmail.com>
2022-09-22 15:31:50 -0700

    pleaseee woork

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,22 @@
 #include "main.h"
 
 /**
- * *_strcat - string concatination  super device
+ * _strcat - Concatenates the string pointed to by @src,
+ * null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
  *
- * @*dest: Our final destination
- * @*src: what it might be ?
- *
- * Return : point toward it, the abyss that consume all of us
+ * Return: A pointer to the destination string @dest.
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i,j,k;
+	int index = 0, dest_len = 0;
 
-	while (i != 2)
-	{
-		if (*(dest + j) == '\0')
-			i++;
-		if (i > 0 && i < 2)
-		{
-			*(dest + j) = *(src + k);
-			k++;
-		}
-		if (*(src + (k-1)) == '\0')
-			i++;
-		j++;
-	}
+	while (dest[index++])
+		dest_len++;
 
- 
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
